a2655e8f8cbb138975094884bee76504a5441b40
sbenkhoud <sahar.ben-khoud@talan.com>
2021-03-11 11:22:40 +0100

    add datepicker

--- src/app/about/components/list-work-of-arts/request/request.component.ts
+++ src/app/about/components/list-work-of-arts/request/request.component.ts
@@ -2,6 +2,7 @@ import { Options } from '@angular-slider/ngx-slider';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { WorkOfArtService } from '@shared/services/work-of-art.service';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-request',
@@ -36,7 +37,15 @@ export class RequestComponent implements OnInit {
   denominations: any;
   dropdownSettings: IDropdownSettings;
   showAdvancedSearchBloc = false;
-  constructor(public WorkOfArtService: WorkOfArtService) {}
+  hoveredDate: NgbDate | null = null;
+
+  fromDate: NgbDate | null;
+  toDate: NgbDate | null;
+  constructor(
+    public WorkOfArtService: WorkOfArtService,
+    private calendar: NgbCalendar,
+    public formatter: NgbDateParserFormatter
+  ) {}
 
   ngOnInit() {
     this.domaine = this.WorkOfArtService.domaine;
@@ -82,5 +91,40 @@ export class RequestComponent implements OnInit {
 
   onValidateRequest() {
     this.validateRequest.emit(true);
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return (
+      this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+    );
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return (
+      date.equals(this.fromDate) ||
+      (this.toDate && date.equals(this.toDate)) ||
+      this.isInside(date) ||
+      this.isHovered(date)
+    );
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
   }
 }
