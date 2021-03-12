3ebf7fac6375796392489c8906eafd26767dad00
Mouna Bouzid <mouna.bouzid@talan.com>
2021-03-12 17:46:18 +0100

    imgDetails

--- src/app/about/components/portail-item-details/portail-item-image/portail-item-image.component.ts
+++ src/app/about/components/portail-item-details/portail-item-image/portail-item-image.component.ts
@@ -7,15 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./portail-item-image.component.scss'],
 })
 export class PortailItemImageComponent implements OnInit {
-  @Output() dataChange = new EventEmitter<string>();
-  show: boolean;
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.show = JSON.parse(params.show);
-      console.log(params.show);
-      this.dataChange.emit(params.show);
-    });
-  }
+  ngOnInit(): void {}
 }
