de3b3608ab50538290b2fa64a0dbeb7f95cecbe7
fares.amdouni <fares.amdouni@talan.com>
2021-03-15 17:40:35 +0100

    update item details  template

--- src/app/about/components/item-details/item-images/item-images.component.ts
+++ src/app/about/components/item-details/item-images/item-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbSlideEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-item-images',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-images.component.scss'],
 })
 export class ItemImagesComponent implements OnInit {
+  slide = 1;
   constructor() {}
 
   ngOnInit(): void {}
+
+  onChange(event: NgbSlideEvent) {
+    switch (event.current) {
+      case 'slide1':
+        this.slide = 1;
+        break;
+      case 'slide2':
+        this.slide = 2;
+        break;
+      case 'slide3':
+        this.slide = 3;
+        break;
+      case 'slide4':
+        this.slide = 4;
+        break;
+    }
+  }
 }
