217100b18b63b88239425838fadba6098d438b58
fares.amdouni <fares.amdouni@talan.com>
2021-04-12 19:40:23 +0100

    update searche page

--- src/app/@shared/services/work-of-art.service.ts
+++ src/app/@shared/services/work-of-art.service.ts
@@ -5,6 +5,16 @@ import { TreeviewItem } from 'ngx-treeview';
   providedIn: 'root',
 })
 export class WorkOfArtService {
+  statusType = [
+    {
+      id: 0,
+      name: 'Dépôt ',
+    },
+    {
+      id: 1,
+      name: 'Prorièté ',
+    },
+  ];
   domaine = [
     {
       id: 0,
@@ -400,6 +410,7 @@ export class WorkOfArtService {
           style: 'Renaissance',
           creationDate: '25/12/1921',
           property: 'Dépôt',
+          depositor: 'depoistor 1',
           visible: true,
         },
         {
@@ -434,6 +445,7 @@ export class WorkOfArtService {
           style: 'Tableau non classés',
           creationDate: '30/11/1920',
           property: 'Dépôt',
+          depositor: 'depoistor 1',
           visible: true,
         },
         {
@@ -472,6 +484,7 @@ export class WorkOfArtService {
           style: 'Renaissance',
           creationDate: '30/11/1920',
           property: 'Dépôt',
+          depositor: 'depoistor 1',
           visible: true,
         },
         {
@@ -513,6 +526,7 @@ export class WorkOfArtService {
           style: 'Sculpture',
           creationDate: '30/11/1920',
           property: 'Dépôt',
+          depositor: 'depoistor 1',
           visible: true,
         },
         {
@@ -531,6 +545,7 @@ export class WorkOfArtService {
           style: 'Tableau non classés',
           creationDate: '21/02/1920',
           property: 'Dépôt',
+          depositor: 'depoistor 1',
           visible: true,
         },
         {
