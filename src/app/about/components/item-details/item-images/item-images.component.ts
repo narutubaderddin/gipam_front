e8c6d360948074759e811a55253c5f9fd1ee1b0f
sbenkhoud <sahar.ben-khoud@talan.com>
2021-04-19 16:25:21 +0200

    add photography part 1

--- src/app/about/components/item-details/item-images/item-images.component.ts
+++ src/app/about/components/item-details/item-images/item-images.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgbModal, NgbModalOptions, NgbSlideEvent } from '@ng-bootstrap/ng-bootstrap';
 import { AddImgModalComponent } from '@app/about/components/item-details/item-images/add-img-modal/add-img-modal.component';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item-images',
@@ -9,16 +9,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./item-images.component.scss'],
 })
 export class ItemImagesComponent implements OnInit {
+  @ViewChild('file') file: any;
   @Input() add = false;
   slide = 1;
   editType = false;
   photographyForm: FormGroup;
   types = ['Identification', 'Autre vue', 'Détail', 'Etat', 'Ancien cliché'];
+  fileToUpload: any;
+  imageUrl: any;
 
   constructor(private modalService: NgbModal, public fb: FormBuilder) {}
   ngOnInit(): void {
     this.initForm();
-    console.log(this.add);
   }
   initForm() {
     this.photographyForm = this.fb.group({
@@ -26,6 +28,35 @@ export class ItemImagesComponent implements OnInit {
       photography: ['', [Validators.required]],
       photographyType: ['', [Validators.required]],
     });
+  }
+  createPhotography(): FormGroup {
+    return this.fb.group({
+      photographyDate: ['', [Validators.required]],
+      photography: ['', [Validators.required]],
+      photographyType: ['', [Validators.required]],
+    });
+  }
+
+  get photographies(): FormArray {
+    return this.photographyForm.get('liens') as FormArray;
+  }
+  addPhotography(): void {
+    this.photographies.push(this.createPhotography());
+  }
+  removePhotography(i: number) {
+    this.photographies.removeAt(i);
+  }
+  handleFileInput(file: FileList) {
+    console.log(file);
+
+    this.fileToUpload = file.item(0);
+
+    //Show image preview
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    };
+    reader.readAsDataURL(this.fileToUpload);
   }
   addImg() {
     const ngbModalOptions: NgbModalOptions = {
@@ -51,4 +82,7 @@ export class ItemImagesComponent implements OnInit {
     }
   }
   editPhotoType() {}
+  addFile() {
+    this.file.nativeElement.click();
+  }
 }
